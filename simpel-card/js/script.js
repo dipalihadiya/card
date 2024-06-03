c38d148b492ed1597be2231f0d9e9c1cf03970bc
dipalihadiya <145538993+dipalihadiya@users.noreply.github.com>
2024-06-03 12:16:40 +0530

    Add files via upload

---
+++ simpel-card/js/script.js
@@ -0,0 +1,200 @@
+let student1 = {
+    name: "John",
+    lname: "vaghela",
+    age: 20,
+    adress : "varacha road surat",
+    grade: 8.5,
+    email : "zqPfM@example.com",
+    phone : 1234567890,
+    city : "surat",
+    country: "India",
+}
+console.log("Student name:", student1.name);
+console.log("Student age:", student1.age);
+console.log("Student grade:", student1.lname);
+
+let h1 = document.getElementById("name");
+h1.innerHTML = "Student Name: " + student1.name
+let par = document.getElementById("lname");
+par.innerHTML = "Last Name: " + student1.lname
+let grad = document.getElementById("age");
+grad.innerHTML = "Age: " + student1.age
+let add = document.getElementById("adress");
+add.innerHTML = "Address: " + student1.adress
+let gra = document.getElementById("grade");
+gra.innerHTML = "Grade: " + student1.grade
+let em = document.getElementById("email");
+em.innerHTML = "Email: " + student1.email
+let ph = document.getElementById("phone");
+ph.innerHTML = "Phone: " + student1.phone
+let cit = document.getElementById("city");
+cit.innerHTML = "City: " + student1.city
+let cou = document.getElementById("country");
+cou.innerHTML = "Country: " + student1.country
+
+
+let student2 = {
+    name: "Raju",
+    lname: "gondaliya",
+    age: 23,
+ 
+}
+console.log("Student name:", student1.name);
+console.log("Student age:", student1.age);
+console.log("Student grade:", student1.lname);
+
+let h11 = document.getElementById("name1");
+h11.innerHTML = "Student Name: " + student2.name
+let par1 = document.getElementById("lname1");
+par1.innerHTML = "Last Name: " + student2.lname
+let grad1 = document.getElementById("age1");
+grad1.innerHTML = "Age: " + student2.age
+
+
+let student3 = {
+    name: "kajal",
+    lname: "dobariya",
+    age: 20,
+ 
+}
+console.log("Student name:", student1.name);
+console.log("Student age:", student1.age);
+console.log("Student grade:", student1.lname);
+
+let h12 = document.getElementById("name2");
+h12.innerHTML = "Student Name: " + student3.name
+let par2 = document.getElementById("lname2");
+par2.innerHTML = "Last Name: " + student3.lname
+let grad2 = document.getElementById("age2");
+grad2.innerHTML = "Age: " + student3.age
+
+
+let student4 = {
+    name: "nisha",
+    lname: "hadiya",
+    age: 19,
+ 
+}
+console.log("Student name:", student1.name);
+console.log("Student age:", student1.age);
+console.log("Student grade:", student1.lname);
+
+let h13 = document.getElementById("name3");
+h13.innerHTML = "Student Name: " + student4.name
+let par3 = document.getElementById("lname3");
+par3.innerHTML = "Last Name: " + student4.lname
+let grad3 = document.getElementById("age3");
+grad3.innerHTML = "Age: " + student4.age
+
+
+let student5 = {
+    name: "Rupali",
+    lname: "Prajapati",
+    age: 23,
+ 
+}
+console.log("Student name:", student1.name);
+console.log("Student age:", student1.age);
+console.log("Student grade:", student1.lname);
+
+let h14 = document.getElementById("name4");
+h14.innerHTML = "Student Name: " + student5.name
+let par4 = document.getElementById("lname4");
+par4.innerHTML = "Last Name: " + student5.lname
+let grad4 = document.getElementById("age4");
+grad4.innerHTML = "Age: " + student5.age
+
+
+let student6 = {
+    name: "Maitri",
+    lname: "Rajput",
+    age: 22,
+ 
+}
+console.log("Student name:", student1.name);
+console.log("Student age:", student1.age);
+console.log("Student grade:", student1.lname);
+
+let h15 = document.getElementById("name5");
+h15.innerHTML = "Student Name: " + student6.name
+let par5 = document.getElementById("lname5");
+par5.innerHTML = "Last Name: " + student6.lname
+let grad5 = document.getElementById("age5");
+grad5.innerHTML = "Age: " + student6.age
+
+
+let student7 = {
+    name: "sonaxi",
+    lname: "vala",
+    age: 20,
+ 
+}
+console.log("Student name:", student1.name);
+console.log("Student age:", student1.age);
+console.log("Student grade:", student1.lname);
+
+let h16 = document.getElementById("name6");
+h16.innerHTML = "Student Name: " + student7.name
+let par6 = document.getElementById("lname6");
+par6.innerHTML = "Last Name: " + student7.lname
+let grad6 = document.getElementById("age6");
+grad6.innerHTML = "Age: " + student7.age
+
+
+let student8 = {
+    name: "mohan",
+    lname: "baliya",
+    age: 24,
+ 
+}
+console.log("Student name:", student1.name);
+console.log("Student age:", student1.age);
+console.log("Student grade:", student1.lname);
+
+let h17 = document.getElementById("name7");
+h17.innerHTML = "Student Name: " + student8.name
+let par7 = document.getElementById("lname7");
+par7.innerHTML = "Last Name: " + student8.lname
+let grad7 = document.getElementById("age7");
+grad7.innerHTML = "Age: " + student8.age
+
+
+let student9 = {
+    name: "krupali",
+    lname: "Mangukiya",
+    age: 23,
+ 
+}
+console.log("Student name:", student1.name);
+console.log("Student age:", student1.age);
+console.log("Student grade:", student1.lname);
+
+let h18 = document.getElementById("name8");
+h18.innerHTML = "Student Name: " + student9.name
+let par8 = document.getElementById("lname8");
+par8.innerHTML = "Last Name: " + student9.lname
+let grad8 = document.getElementById("age8");
+grad8.innerHTML = "Age: " + student9.age
+
+
+
+let student10 = {
+    name: "mansi",
+    lname: "jain",
+    age: 23,
+ 
+}
+console.log("Student name:", student1.name);
+console.log("Student age:", student1.age);
+console.log("Student grade:", student1.lname);
+
+let h19 = document.getElementById("name9");
+h19.innerHTML = "Student Name: " + student10.name
+let par9 = document.getElementById("lname9");
+par9.innerHTML = "Last Name: " + student10.lname
+let grad9 = document.getElementById("age9");
+grad9.innerHTML = "Age: " + student10.age
+
+
+
+
